6ce650d5b7a5d8d419af616e373f91d772450a00
Tim Swihart <chronoslynx@gmail.com>
2014-04-12 10:07:50 -0400

    Making driver unzip more general and adding check to make sure we unzip only zip files

--- batch_converter/driver.rb
+++ batch_converter/driver.rb
@@ -1,18 +1,22 @@
-
+#!/usr/bin/env ruby
 class Driver
-	def unzip
-		Dir.foreach('.') do |item|
+	def unzip(dir)
+		Dir.foreach(dir) do |item|
 			next if item == '.' or item == '..'
+			next if item.split('.').last != 'zip'
+
 			if item.split('@').length == 2
 				`unzip #{item} -d #{item[0..-5]}`
 				puts "Extracted #{item[0..-5]}, ready to hydrate item model"
+				# should call other code, then clean up the extracted zip file here
 		  	end
 			if item.split('aip').length > 1
 				`unzip #{item} -d #{item[0..-5]}`
 				puts "Extracted #{item[0..-5]}, ready to create collection model"
+				# should call other code, then clean up the extracted zip file here
 			end
 		end
 		puts "All items extracted, ready to augment collection with items"
 	end
 end
-Driver.new().unzip()
+Driver.new().unzip('.')
