374b0b6a4346bbda52327751c89c5d534d9ce936
Tim Swihart <chronoslynx@gmail.com>
2014-04-14 16:31:34 -0400

    Unzipping -> basic collection works

--- batch_converter/driver.rb
+++ batch_converter/driver.rb
@@ -1,20 +1,37 @@
 #!/usr/bin/env ruby
+require './collection_builder'
+require 'fileutils'
 class Driver
 	def unzip(dir)
 		Dir.foreach(dir) do |item|
 			next if item == '.' or item == '..'
 			next if item.split('.').last != 'zip'
+			basename = File.basename(item, '.zip')
+			# folder_loc = 
+
+			# unzip folder
+			`unzip #{File.join(dir, item)} -d #{File.join(dir, basename)}`
+
+			xmlfname = File.join(dir, basename, 'mets.xml')
+
 
 			if item.split('@').length == 2
-				`unzip #{item} -d #{item[0..-5]}`
-				puts "Extracted #{item[0..-5]}, ready to hydrate item model"
+				puts "Extracted #{basename}, ready to hydrate item model"
 				# should call other code, then clean up the extracted zip file here
+				# this should call the BUILD_MODEL code
 		  	end
 			if item.split('aip').length > 1
-				`unzip #{item} -d #{item[0..-5]}`
-				puts "Extracted #{item[0..-5]}, ready to create collection model"
+				# `unzip #{File.join(dir, item)} -d #{File.join(dir, basename)}`
+				# puts "Extracted #{item[0..-5]}, ready to create collection model"
 				# should call other code, then clean up the extracted zip file here
+				# this should call BUILD_MODEL but  with the flattened collection model
+				# puts File.join(dir, basename, basename+'.zip')
+				c = CollectionBuilder.build(xmlfname)
+				puts c.to_s
 			end
+
+			# Clean up directories after
+			FileUtils.rm_rf(File.join(dir, basename))
 		end
 		puts "All items extracted, ready to augment collection with items"
 	end
